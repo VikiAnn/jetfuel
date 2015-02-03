85781f47841f5941cbd0f6e030634e8ceb12b1f4
Viki Harrod <victoria.harrod@gmail.com>
2015-02-03 14:52:35 -0700

    add url index route, first passing spec

---
+++ spec/features/url_listing_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+describe "url list", type: :feature do
+  it "lists urls" do
+    url1 = Url.create(given_url: "www.first.example.com")
+    url2 = Url.create(given_url: "www.second.example.com")
+
+    visit urls_path
+
+    expect(page).to have_content(url1.given_url)
+    expect(page).to have_content(url2.given_url)
+  end
+end
