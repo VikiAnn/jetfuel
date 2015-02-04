9aa5fd6c4ac78d5728bd55a87fbf1839ae0956ea
Viki Harrod <victoria.harrod@gmail.com>
2015-02-04 15:39:05 -0700

    make hound happy

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,8 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe Url, :type => :model do
+RSpec.describe Url, type: :model do
   it "can produce a shortened url" do
-    url = Url.create!(given_url: "www.example.com/somewhere/somewhere/somewhere")
+    url = Url.create!(given_url: "www.example.com/something/somewhere/somehow")
 
     expect(url.slug).to eq(url.id.to_s(36))
   end
